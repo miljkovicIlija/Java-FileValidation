0b59c635bf869b2747ada92df03f80d069054268
miljkovicIlija <48562888+miljkovicIlija@users.noreply.github.com>
2019-03-14 18:07:57 +0100

    Add files via upload

---
+++ src/filevalidationproject/Validator.java
@@ -0,0 +1,15 @@
+
+package filevalidationproject;
+
+
+public interface Validator {
+    
+    
+    public boolean isAlpha(String textToCheck); 
+    
+    public boolean isDigit(String textToCheck); 
+    
+    public boolean isASCII(String textToCheck);
+        
+    
+}
